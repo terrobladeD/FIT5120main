9a4c9b431b9df0015927ab65bcad9a2c7fb8dd95
terrobladeD <terrobladediao@gmail.com>
2023-04-15 10:48:40 +1000

    hasg router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import JobCheck from './pages/JobCheck.js';
 import IndustryAnalysis from './pages/IndustryAnalysis.js';
